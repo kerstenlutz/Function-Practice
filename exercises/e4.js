d51576f419f23a6675e6d7b5fb9a58262e8e02a8
Kersten Lutz <kerstenlutz@hotmail.com>
2024-03-13 15:40:20 -0400

    Exercise 4 commit

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,9 +9,9 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
+const splitFirstAndLastNames = (str) => str.split(' ');
+
+console.log(splitFirstAndLastNames('John Smith'));
 
 
 /** =========================
@@ -20,11 +20,11 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+const personAge = (personObject) => personObject.age;
 
+console.log(personAge({ id: 1, name: 'Someone', age: 32}));
 
+ 
 /** =========================
  * The isNameInArray function takes an array and string of name as the arguments.
  * The function returns the boolean (true/false) if the name exists in the given array
@@ -32,9 +32,10 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
+const isNameInArray = (arr, name) => arr.includes(name);
+
+console.log(isNameInArray(['Jon', 'Michael', 'Andrey'], 'Michael'));
+console.log(isNameInArray(['Jon', 'Michael', 'Andrey'], 'James'));
 
 
 /** =========================
@@ -44,16 +45,18 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
-  const timer = setInterval(function() {
-    if (i < max) {
-      console.log(++i);
-    } else {
-      clearInterval(timer);
-    }
-  }, 1000);
+  const timer = setInterval = () => {
+      if (i < max ) {
+          console.log(++i);
+      } else {
+          clearInterval(timer);
+      }
+  } 
 }
+
+console.log(logSecondsUpToMax(1000));
 
 
 
